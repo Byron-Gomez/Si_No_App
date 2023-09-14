64f11d146d76f103d7b2c6089de4dc8be48ad87b
SpringBoot-Crud_Cliente <byronxgomez127@gmail.com>
2023-09-14 11:59:27 -0600

    agrege comentarios

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,31 +1,36 @@
-
 import 'package:flutter/material.dart';
 
+// Definición del color personalizado
 const Color _customColor = Color(0xFF49149F);
 
+// Lista de colores temáticos
 const List<Color> _colorThemes = [
-_customColor,
-Colors.blue,
-Colors.teal,
-Colors.green,
-Colors.yellow,
-Colors.orange,
-Colors.pink,
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
 ];
 
+// Clase que representa un tema de la aplicación
+class AppTheme {
+  final int selectedColor; // Índice del color seleccionado
 
-class AppTheme{
-final int selectedColor;
+  // Constructor de la clase
+  AppTheme({this.selectedColor = 0})
+      : assert(
+            selectedColor >= 0 &&
+                selectedColor <= _colorThemes.length - 1,
+            'Colors must be between 0 and ${_colorThemes.length}');
 
-AppTheme({this.selectedColor = 0})
-    :assert(selectedColor >= 0&& selectedColor <= _colorThemes.length -1,
-    'Colors must be between 0 and ${_colorThemes.length}');
-  
-  ThemeData theme(){
+  // Método para obtener el tema de la aplicación
+  ThemeData theme() {
     return ThemeData(
-      useMaterial3: true,
-      colorSchemeSeed: _colorThemes[selectedColor],
-      //brightness: Brightness.dart
+      useMaterial3: true, // Habilita el uso de Material3 (si se usa en la versión de Flutter correspondiente)
+      colorSchemeSeed: _colorThemes[selectedColor], // Define el color principal del tema
+      //brightness: Brightness.dart // Opción para ajustar el brillo del tema (actualmente comentada)
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Este widget es la raíz de tu aplicación.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Si No APP',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme( selectedColor: 2).theme(),
+      debugShowCheckedModeBanner: false, // Deshabilita el banner de depuración en la esquina superior derecha.
+      theme: AppTheme(selectedColor: 2).theme(), // Establece el tema de la aplicación usando la clase AppTheme.
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
-
+          title: const Text('Material App Bar'), // Título de la barra de la aplicación.
         ),
-        body: Center(child: FilledButton.tonal(
-          onPressed: (){},
-         child: const Text('Click me'))),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: () {},
+            child: const Text('Click me'), // Contenido del botón.
+          ),
+        ),
       ),
-       
     );
   }
 }
